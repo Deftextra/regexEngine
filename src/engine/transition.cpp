94c703c7a83a7e66fcd379cf907a8397365c167c
Radwan Suufi <rd.90@hotmail.com>
2020-06-01 19:16:34 +0100

    Refactor transition files

---
+++ src/engine/transition.cpp
@@ -0,0 +1,37 @@
+#include "transition.h"
+
+
+template <typename T>
+engine::Transition<T>::Transition()
+  :head{new T{}}, symbol{0}, isEpsilon{true}{}
+
+
+template <typename T>
+engine::Transition<T>::Transition(char s)
+  :head{new T{}}, symbol{s}, isEpsilon{false} {}
+
+template <typename T>
+engine::Transition<T>::~Transition()
+{
+  delete head;
+}
+
+template <typename T>
+inline T*  engine::Transition<T>::getHead() const
+{
+  return head;
+}
+
+template <typename T>
+inline char engine::Transition<T>::getSymbol() const
+{
+  return symbol;
+}
+
+template <typename T>
+inline bool engine::Transition<T>::getIsEpsilon() const
+{
+  return isEpsilon;
+}
+
+template class engine::Transition<int>;
---
+++ src/engine/transition.h
@@ -0,0 +1,31 @@
+#ifndef TRANSITION_H
+#define TRANSITION_H
+
+#include <iostream>
+#include <stdexcept>
+
+
+namespace engine 
+{
+  template <typename T>
+    class Transition 
+    {
+      public:
+        Transition();
+        explicit Transition(char s);
+
+        ~Transition();
+
+        char getSymbol() const;
+        T* getHead() const;
+        bool getIsEpsilon() const;
+
+      private:
+        T* head;
+        char symbol;
+        bool isEpsilon;
+
+    };
+}
+
+#endif //TRANSITION_H
